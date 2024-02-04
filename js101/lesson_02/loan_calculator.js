62df858ed7488e2ec97d1e01bf6e900b78739dc6
KevinPhDev <kevinphdev@outlook.com>
2024-02-04 22:47:49 +1100

    Loan calculator gets user inputs then calculates and prints monthly interest payment in $

--- js101/lesson_02/loan_calculator.js
+++ js101/lesson_02/loan_calculator.js
@@ -1,7 +1,19 @@
-let loanAmount = 100000;
-let annualInterestRate = 0.06;
-let loanDurationInYears = 5.3;
+const readline = require("readline-sync");
 
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt("Calculate your monthly interest rate");
+
+prompt("What is your loan amount in $?");
+let loanAmount = Number(readline.question());
+
+prompt("What is your annual interest rate in %?");
+let annualInterestRate = Number(readline.question() / 100);
+
+prompt("What is your loan duration in years?");
+let loanDurationInYears = Number(readline.question());
 
 const monthlyInterestRate = annualInterestRate / 12;
 const loanDurationInMonths = loanDurationInYears * 12;
@@ -9,4 +21,4 @@ const loanDurationInMonths = loanDurationInYears * 12;
 const monthlyPayment = loanAmount * (monthlyInterestRate /
 (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))));
 
-console.log(monthlyPayment);
+prompt(`This your your monthly payment $${monthlyPayment.toFixed(2)}`);
