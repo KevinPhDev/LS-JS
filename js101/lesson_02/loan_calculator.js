a95c00ade207b7af673e103f0d17768693446fa0
KevinPhDev <kevinphdev@outlook.com>
2024-02-04 22:33:53 +1100

    Loan calculator converts interest rates from annual to monthly and loan duration from years to months

--- js101/lesson_02/loan_calculator.js
+++ js101/lesson_02/loan_calculator.js
@@ -1,6 +1,10 @@
 let loanAmount = 100000;
-let monthlyInterestRate = 0.005;
-let loanDurationInMonths = 120;
+let annualInterestRate = 0.06;
+let loanDurationInYears = 5.3;
+
+
+const monthlyInterestRate = annualInterestRate / 12;
+const loanDurationInMonths = loanDurationInYears * 12;
 
 const monthlyPayment = loanAmount * (monthlyInterestRate /
 (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))));
