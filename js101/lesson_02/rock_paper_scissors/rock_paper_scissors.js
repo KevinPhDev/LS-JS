d5661a3ee885de1ccf62325f5337ef9226bd64ab
KevinPhDev <kevinphdev@outlook.com>
2024-02-08 16:12:34 +1100

    Game continues or ends when player decides

--- js101/lesson_02/rock_paper_scissors/rock_paper_scissors.js
+++ js101/lesson_02/rock_paper_scissors/rock_paper_scissors.js
@@ -5,27 +5,38 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let choice = readline.question();
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
 
-while (!VALID_CHOICES.includes(choice)) {
-  prompt('Invalid choice');
-  choice = readline.question();
-}
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt('Invalid choice');
+    choice = readline.question();
+  }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
 
-prompt(`Player: ${choice} Computer: ${computerChoice}`);
+  prompt(`Player: ${choice} Computer: ${computerChoice}`);
 
-if ((choice === 'rock' && computerChoice === 'scissors') ||
-    (choice === 'paper' && computerChoice === 'rock') ||
-    (choice === 'scissors' && computerChoice === 'paper')) {
-  prompt('You win!');
-} else if ((choice === 'rock' && computerChoice === 'paper') ||
-           (choice === 'paper' && computerChoice === 'scissors') ||
-           (choice === 'scissors' && computerChoice === 'rock')) {
-  prompt('Computer wins!');
-} else {
-  prompt("It's a tie");
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You win!');
+  } else if ((choice === 'rock' && computerChoice === 'paper') ||
+              (choice === 'paper' && computerChoice === 'scissors') ||
+              (choice === 'scissors' && computerChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("It's a tie");
+  }
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
 }
