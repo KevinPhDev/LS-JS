5d60cf4ce62ad885ff54e63c0ee5c5d514c9a1d2
KevinPhDev <kevinphdev@outlook.com>
2024-02-08 16:05:21 +1100

    With win condition logic, game determines and logs the winner

--- js101/lesson_02/rock_paper_scissors/rock_paper_scissors.js
+++ js101/lesson_02/rock_paper_scissors/rock_paper_scissors.js
@@ -17,3 +17,15 @@ let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
 let computerChoice = VALID_CHOICES[randomIndex];
 
 prompt(`Player: ${choice} Computer: ${computerChoice}`);
+
+if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'paper' && computerChoice === 'rock') ||
+    (choice === 'scissors' && computerChoice === 'paper')) {
+  prompt('You win!');
+} else if ((choice === 'rock' && computerChoice === 'paper') ||
+           (choice === 'paper' && computerChoice === 'scissors') ||
+           (choice === 'scissors' && computerChoice === 'rock')) {
+  prompt('Computer wins!');
+} else {
+  prompt("It's a tie");
+}
